0b412c1da067d8dc69e62d894023239065afa141
mdunser <marc.dunser@cern.ch>
2020-03-02 14:09:02 +0100

    setup new for root on lxplus with centos7

--- etc/scripts/setup.sh
+++ etc/scripts/setup.sh
@@ -5,11 +5,7 @@ if [[ `uname -n` == *"lxplus"* ]]; then
     optVer=x86_64-slc6-gcc49-opt
     optVer=x86_64-slc6-gcc48-opt
 
-## install modern version of ROOT
-    LCG=/afs/cern.ch/sw/lcg/
-    ROOTSYS=$LCG/app/releases/ROOT/6.06.06/$optVer/root/
-    . $LCG/external/gcc/4.8.0/$optVer/setup.sh
-    . $ROOTSYS/bin/thisroot.sh
+    . /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.16.00/x86_64-centos7-gcc48-opt/bin/thisroot.sh 
 
 ## add python lib
     . /opt/rh/python27/enable
