8449f8ab73c9c051d368a9aec084628e39c13931
Davide Bruschini <davide.bruschini@cern.ch>
2022-12-02 12:00:54 +0100

    Hadd workaround

--- libCpp/FileMerger.C
+++ libCpp/FileMerger.C
@@ -6,13 +6,23 @@
 
 #include <vector>
 #include <string>
-#include <unordered_map>
+#include <sstream>
+#include <stdexcept>
 
-void FileMerger(const char* filename,std::vector<std::string> names) {
+void FileMerger(const char* NumberOfBins, const char* filename, std::vector<std::string> names) {
+	istringstream myStream(NumberOfBins);
+	unsigned int bins = 0;
+	myStream >> bins;
+	if (bins!=names.size()) throw std::runtime_error("The number of RooFit output files does not correspond to the number of bins.");
 	TFile *outputfile=new TFile(filename,"RECREATE");
 	outputfile->cd();
 	for (unsigned int i=0; i!=names.size(); i++) {
 		TFile *file=new TFile(names[i].c_str());
+		if (file->IsZombie()) {
+			std::string string(names[i]);
+			string+=std::string(" is corrupted.");
+			throw std::runtime_error(string.c_str());
+		}
 		TObject *obj;
 		TKey *key;
 		TIter next( file->GetListOfKeys());
@@ -28,9 +38,15 @@ void FileMerger(const char* filename,std::vector<std::string> names) {
 
 int main(int argc, char **argv) {
 	std::vector<std::string> names;
-	for (int i=2; i!=argc; i++) {
+	for (int i=3; i!=argc; i++) {
 		names.push_back(std::string(argv[i]));
 	}
-	FileMerger(argv[1],names);
+	try {
+		FileMerger(argv[1],argv[2],names);
+	}
+	catch (std::runtime_error& exception) {
+		std::cerr<<exception.what()<<"\n";
+		return -1;
+	}
 	return 0;
 }
